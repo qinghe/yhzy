b8a1385771024657e484e6603a9b368eac5a7c2f
zhang ze <david@localhost.localdomain>
2015-05-05 02:36:54 -0700

    remove temp from github

--- app/temp/temp_c/%%7D^7D5^7D515780%%foot.html.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2013-11-07 15:55:48
-         compiled from main/common/foot.html */ ?>
- <!--footer-->
-<div id="footer">
-<?php echo $this->_tpl_vars['inis']['down']; ?>
-
-</div>
- <!--footer-->
