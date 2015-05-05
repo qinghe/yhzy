db2c0eac13e6572501074c69ed7d44869b3b4fd1
zhang ze <david@localhost.localdomain>
2015-05-05 02:36:41 -0700

    remove temp from github

--- app/temp/temp_c/%%2B^2B1^2B13CFA8%%main.html.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2015-03-30 15:04:46
-         compiled from main/common/main.html */ ?>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => "main/".($this->_tpl_vars['mainPage']).".html", 'smarty_include_vars' => array()));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
